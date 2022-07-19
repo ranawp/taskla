ba9ab5f30119a3b1eeb10fad3d5342bbb919fd10
ranawp <ranasaha4224#gmail.com>
2022-07-19 14:31:35 +0600

    button added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <p class="text-blue-600">The quick brown fox...</p>
+      <button class="btn btn-primary">Button</button>
+
     </div>
   );
 }
