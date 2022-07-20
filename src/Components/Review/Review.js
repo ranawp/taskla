673d9414ff9f92d7bec949d32ffd371ff3baa8bc
Junayed5 <mdjunayed352003@gmail.com>
2022-07-20 23:28:50 +0600

    new slider

--- src/Components/Review/Review.js
+++ src/Components/Review/Review.js
@@ -1,79 +1,127 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+
+import "./Review.css";
+
+import { EffectCoverflow, Pagination } from "swiper";
 
 const Review = () => {
     return (
         <div>
+            <Swiper
+                effect={"coverflow"}
+                grabCursor={true}
+                centeredSlides={true}
+                slidesPerView={"auto"}
+                coverflowEffect={{
+                    rotate: 50,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 1,
+                    slideShadows: true,
+                }}
+                pagination={true}
+                modules={[EffectCoverflow, Pagination]}
+                className="mySwiper"
+            >
 
-
-            <div class="h-[500px] bg-base-200">
-
-                <h1 className='text-center text-4xl my-10 font-bold'>What People Are Saying</h1>
-
-                <div class="carousel w-full">
-                    <div id="item1" class="carousel-item w-full flex justify-center">
-                        {/* <img src="https://placeimg.com/800/200/arch" class="w-full" /> */}
-                        <div class="card card-side bg-base-100 shadow-xl w-3/4">
-                            <figure><img src="https://placeimg.com/200/280/arch" alt="Movie" /></figure>
-                            <div class="card-body">
-                                <h2 class="card-title">New movie is released!</h2>
-                                <p>Click the button to watch on Jetflix app.</p>
-                                <div class="card-actions justify-end">
-                                    <button class="btn btn-primary">Watch</button>
+                <SwiperSlide>
+                    <div class="card w-96 bg-base-100 shadow-xl">
+                        <figure class="px-10 pt-10">
+                            <div class="avatar">
+                                <div class="w-24 rounded-full">
+                                    <img src="https://placeimg.com/192/192/people" />
                                 </div>
                             </div>
+                        </figure>
+                        <div class="card-body items-center text-center">
+                            <h2 class="card-title">Shoes!</h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
                         </div>
                     </div>
-                    <div id="item2" class="carousel-item w-full flex justify-center">
-
-                        <div class="card card-side bg-base-100 shadow-xl w-3/4">
-                            <figure><img src="https://placeimg.com/200/280/arch" alt="Movie" /></figure>
-                            <div class="card-body">
-                                <h2 class="card-title">New movie is released!</h2>
-                                <p>Click the button to watch on Jetflix app.</p>
-                                <div class="card-actions justify-end">
-                                    <button class="btn btn-primary">Watch</button>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div class="card w-96 bg-base-100 shadow-xl">
+                        <figure class="px-10 pt-10">
+                        <div class="avatar">
+                                <div class="w-24 rounded-full">
+                                    <img src="https://placeimg.com/192/192/people" />
                                 </div>
                             </div>
+                        </figure>
+                        <div class="card-body items-center text-center">
+                            <h2 class="card-title">Shoes!</h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
                         </div>
-
                     </div>
-                    <div id="item3" class="carousel-item w-full flex justify-center">
-
-                        <div class="card card-side bg-base-100 shadow-xl w-3/4">
-                            <figure><img src="https://placeimg.com/200/280/arch" alt="Movie" /></figure>
-                            <div class="card-body">
-                                <h2 class="card-title">New movie is released!</h2>
-                                <p>Click the button to watch on Jetflix app.</p>
-                                <div class="card-actions justify-end">
-                                    <button class="btn btn-primary">Watch</button>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div class="card w-96 bg-base-100 shadow-xl">
+                        <figure class="px-10 pt-10">
+                        <div class="avatar">
+                                <div class="w-24 rounded-full">
+                                    <img src="https://placeimg.com/192/192/people" />
                                 </div>
                             </div>
+                        </figure>
+                        <div class="card-body items-center text-center">
+                            <h2 class="card-title">Shoes!</h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
                         </div>
-
                     </div>
-                    <div id="item4" class="carousel-item w-full flex justify-center">
-
-                        <div class="card card-side bg-base-100 shadow-xl w-3/4">
-                            <figure><img src="https://placeimg.com/200/280/arch" alt="Movie" /></figure>
-                            <div class="card-body">
-                                <h2 class="card-title">New movie is released!</h2>
-                                <p>Click the button to watch on Jetflix app.</p>
-                                <div class="card-actions justify-end">
-                                    <button class="btn btn-primary">Watch</button>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div class="card w-96 bg-base-100 shadow-xl">
+                        <figure class="px-10 pt-10">
+                        <div class="avatar">
+                                <div class="w-24 rounded-full">
+                                    <img src="https://placeimg.com/192/192/people" />
                                 </div>
                             </div>
+                        </figure>
+                        <div class="card-body items-center text-center">
+                            <h2 class="card-title">Shoes!</h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
                         </div>
-
                     </div>
-                </div>
-                <div class="flex justify-center w-full py-2 gap-2">
-                    <a href="#item1" class="btn btn-xs">1</a>
-                    <a href="#item2" class="btn btn-xs">2</a>
-                    <a href="#item3" class="btn btn-xs">3</a>
-                    <a href="#item4" class="btn btn-xs">4</a>
-                </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div class="card w-96 bg-base-100 shadow-xl">
+                        <figure class="px-10 pt-10">
+                        <div class="avatar">
+                                <div class="w-24 rounded-full">
+                                    <img src="https://placeimg.com/192/192/people" />
+                                </div>
+                            </div>
+                        </figure>
+                        <div class="card-body items-center text-center">
+                            <h2 class="card-title">Shoes!</h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div class="card w-96 bg-base-100 shadow-xl">
+                        <figure class="px-10 pt-10">
+                        <div class="avatar">
+                                <div class="w-24 rounded-full">
+                                    <img src="https://placeimg.com/192/192/people" />
+                                </div>
+                            </div>
+                        </figure>
+                        <div class="card-body items-center text-center">
+                            <h2 class="card-title">Shoes!</h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                        </div>
+                    </div>
+                </SwiperSlide>
+            </Swiper>
 
-            </div>
+
         </div>
     );
 };
