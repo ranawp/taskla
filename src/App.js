d2382cee8753eb32378422341749a02b4d535d3c
hridoy002 <hridoyalhasan2@gmail.com>
2022-07-21 13:27:50 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Navbar from './Share/Navbar';
 
 function App() {
   return (
-    <div>
+    <div className="max-w-7xl mx-auto">
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
