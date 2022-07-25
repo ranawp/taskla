23379c3ea78189b8375a438554075064b22a39e9
parvez13913 <parvezz13913@gmail.com>
2022-07-25 14:41:49 -0700

    Register Component create

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Login = () => {
     return (
         <div>
-            <h1>This is Login page</h1>
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt quidem nostrum temporibus nulla ea delectus architecto perferendis quod ut dolorem quo molestiae, fuga officiis vitae facilis perspiciatis nisi reiciendis reprehenderit.</h1>
         </div>
     );
 };
