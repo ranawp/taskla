b7fd4937ae0d17392eb91ee2c50965dfc4513d3d
Md Junayed <97009486+Junayed5@users.noreply.github.com>
2022-08-03 21:03:03 +0600

    Update SingleTasks.js

--- src/Components/Dashboard/Outlet/MyTasks/SingleTasks.js
+++ src/Components/Dashboard/Outlet/MyTasks/SingleTasks.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 const SingleTasks = ({ task, index, setModal}) => {
     // console.log(task)
-const SingleTasks = ({ task, index }) => {
-    console.log(task)
+
     return (
         <tr>
             <th>{index + 1}</th>
@@ -14,9 +13,6 @@ const SingleTasks = ({ task, index }) => {
             <td>{task.taskDeadline}</td>
             <td><label htmlFor="my-modal-3" className="btn modal-button bg-blue-700 border-0 px-3 py-1 button  rounded text-white"  onClick={() => setModal(task)}>Submit</label></td>
         </tr >
-            <td><input className=' bg-blue-700 border-0 px-3 py-1 button  rounded text-white' type="submit" value="Submit" /></td>
-
-        </tr>
     );
 };
 
