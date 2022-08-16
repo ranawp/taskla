33a88ec3aedf40d09e4d10e05795dfecba1772d0
ranawp <ranasaha4224#gmail.com>
2022-08-16 23:04:44 +0600

    my task ui change

--- src/Components/Dashboard/Outlet/MyTasks/MyTask.js
+++ src/Components/Dashboard/Outlet/MyTasks/MyTask.js
@@ -5,17 +5,24 @@ import FeedbackDetails from './FeedbackDetails';
 import SingleTasks from './SingleTasks';
 import TaskDetails from './TaskDetails';
 import TaskModal from './TaskModal';
+import './MyTask.css';
+
 
 const MyTask = () => {
     const [singleTask, setSingleTask] = useState([]);
-    const [singleTasks, setSingleTasks] = useState([]);
-    const [taskFeedback, setTaskFeedback] = useState([]);
-    const [modal, setModal] = useState({});
-    const [modalDetails, setModalDetails] = useState(null);
-    const [feedbackModal, setFeedbackModal] = useState(null)
-    // const [reload, setReload] = useState(false);
+    const [taskData, setTaskData] = useState([]);
+    const [taskSubmit, setTaskSubmit] = useState([]);
+    const [toogle, setToogle] = useState(true);
+    const [submitOne, setSubmitOne] = useState(false);
+
     const [user] = useAuthState(auth)
     const email = user?.email;
+
+    // console.log(taskData)
+    // console.log(submit)
+    // console.log(submitOne)
+
+
 
     useEffect(() => {
         fetch('http://localhost:5000/alltasks')
@@ -23,65 +30,170 @@ const MyTask = () => {
             .then(data => setSingleTask(data))
     }, [])
 
-    const [marks, setMarks] = useState([]);
-    useEffect(() => {
-        fetch(`http://localhost:5000/allMarks/${email}`)
-            .then(res => res.json())
-            .then(data => setMarks(data))
-    }, [])
+    // const [marks, setMarks] = useState([]);
+    // useEffect(() => {
+    //     fetch(`http://localhost:5000/allMarks/${email}`)
+    //         .then(res => res.json())
+    //         .then(data => setMarks(data))
+    // }, [])
 
+
+    // console.log(taskData)
+    // console.log(taskSubmit)
 
 
     return (
-        <div>
-            <h4 className='text-dark font-bold text-2xl'>My Tasks</h4>
-            <div className="overflow-x-auto">
-                <table className="table w-full">
+        <>
+            <h1 className='text-2xl text-center mt-10'>Your Assignment</h1>
+            <section className="grid grid-cols-2 px-10 mt-5 task-list">
 
-                    <thead>
-                        <tr>
-                            <th>Serial</th>
-                            <th>Task Name</th>
-                            <th>Task no.</th>
-                            <th>Task details</th>
-                            <th>Task Created Time</th>
+                <div className='w-4/5 border h-80 overflow-y-auto' >
+                    {singleTask.map((singleTask) =>
+                        <SingleTasks
+                            key={singleTask._id}
+                            singleTask={singleTask}
+                            setTaskData={setTaskData}
+                            setTaskSubmit={setTaskSubmit}
+                            setToogle={setToogle}
+                            setSubmitOne={setSubmitOne}
+                            toogle={toogle}
 
-                            <th>DeadLine</th>
-                            {/* <th>Result</th> */}
-                            {/* <th>Feedback</th>  */}
-                            <th>Submit</th>
-                        </tr>
-                    </thead>
+                        ></SingleTasks>
+                    )}
+                </div>
 
-                    <tbody>
-                        {
-                            singleTask.map((singleTask, index) => <SingleTasks
-
-                                key={singleTask._id}
-                                task={singleTask}
-                                index={index}
-                                setModal={setModal}
-                                setModalDetails={setModalDetails}
-                                setFeedbackModal={setFeedbackModal}
-                                taskFeedback={taskFeedback}
-                                setMarks={setMarks}
-                                marks={marks}
+                <div className="overflow-y-auto border h-80">
 
 
-                            // reload={reload}
-                            // setReload={setReload}
-                            ></SingleTasks>)
+                    {toogle &&
+                        <div>
+                            <>
+                                <h1 className="title">Task Name:{taskData.taskName}</h1>
+                                <p className="details">Task no:{taskData.taskSerial}</p>
+                                <p class="details">Task Posted time:{taskData.questionDeliverDate}</p>
+                                <p className="details">Deadline:{taskData.taskDeadline}</p>
+                                <p className="details">Task Details : {taskData.taskMassage} </p> </>
+
+
+                        </div>
+                    }
+                    <>
+                        {!toogle &&
+                            <textarea placeholder='submit your answer' name="" id="" cols="30" rows="10"></textarea>
                         }
-                    </tbody>
-                    {modalDetails && <TaskDetails modalDetails={modalDetails} ></TaskDetails>}
-                    {feedbackModal && <FeedbackDetails task={singleTask} feedbackModal={feedbackModal}></FeedbackDetails>}
+                    </>
 
-                </table>
-            </div>
-            {modal && <TaskModal modal={modal} />}
 
-        </div>
+
+
+
+
+
+
+                </div>
+
+
+
+            </section>
+        </>
     );
 };
 
 export default MyTask;
+
+
+
+
+
+
+
+
+
+// /////////////////////
+// import React, { useEffect, useState } from 'react';
+// import { useAuthState } from 'react-firebase-hooks/auth';
+// import auth from '../../../../firebase.init';
+// import FeedbackDetails from './FeedbackDetails';
+// import SingleTasks from './SingleTasks';
+// import TaskDetails from './TaskDetails';
+// import TaskModal from './TaskModal';
+
+// const MyTask = () => {
+//     const [singleTask, setSingleTask] = useState([]);
+//     const [singleTasks, setSingleTasks] = useState([]);
+//     const [taskFeedback, setTaskFeedback] = useState([]);
+//     const [modal, setModal] = useState({});
+//     const [modalDetails, setModalDetails] = useState(null);
+//     const [feedbackModal, setFeedbackModal] = useState(null)
+//     // const [reload, setReload] = useState(false);
+//     const [user] = useAuthState(auth)
+//     const email = user?.email;
+
+//     useEffect(() => {
+//         fetch('http://localhost:5000/alltasks')
+//             .then(res => res.json())
+//             .then(data => setSingleTask(data))
+//     }, [])
+
+//     const [marks, setMarks] = useState([]);
+//     useEffect(() => {
+//         fetch(`http://localhost:5000/allMarks/${email}`)
+//             .then(res => res.json())
+//             .then(data => setMarks(data))
+//     }, [])
+
+
+
+//     return (
+//         <div>
+//             <h4 className='text-dark font-bold text-2xl'>My Tasks</h4>
+//             <div className="overflow-x-auto">
+//                 <table className="table w-full">
+
+//                     <thead>
+//                         <tr>
+//                             <th>Serial</th>
+//                             <th>Task Name</th>
+//                             <th>Task no.</th>
+//                             <th>Task details</th>
+//                             <th>Task Created Time</th>
+
+//                             <th>DeadLine</th>
+//                             {/* <th>Result</th> */}
+//                             {/* <th>Feedback</th>  */}
+//                             <th>Submit</th>
+//                         </tr>
+//                     </thead>
+
+//                     <tbody>
+//                         {
+//                             singleTask.map((singleTask, index) => <SingleTasks
+
+//                                 key={singleTask._id}
+//                                 task={singleTask}
+//                                 index={index}
+//                                 setModal={setModal}
+//                                 setModalDetails={setModalDetails}
+//                                 setFeedbackModal={setFeedbackModal}
+//                                 taskFeedback={taskFeedback}
+//                                 setMarks={setMarks}
+//                                 marks={marks}
+
+
+//                             // reload={reload}
+//                             // setReload={setReload}
+//                             ></SingleTasks>)
+//                         }
+//                     </tbody>
+//                     {modalDetails && <TaskDetails modalDetails={modalDetails} ></TaskDetails>}
+//                     {feedbackModal && <FeedbackDetails task={singleTask} feedbackModal={feedbackModal}></FeedbackDetails>}
+
+//                 </table>
+//             </div>
+//             {modal && <TaskModal modal={modal} />}
+
+//         </div>
+//     );
+// };
+
+// export default MyTask;
--- src/Components/Dashboard/Outlet/MyTasks/SingleTasks.js
+++ src/Components/Dashboard/Outlet/MyTasks/SingleTasks.js
@@ -1,22 +1,88 @@
 import React from 'react';
+import './MyTask.css';
 
-const SingleTasks = ({ task, index, setModalDetails, setModal, setFeedbackModal, taskFeedback, singleMark, marks, setMarks }) => {
+const SingleTasks = ({ singleTask, setTaskData, setTaskSubmit, setToogle, setSubmitOne, toogle }) => {
+
+    const handleClick = task => {
+        setTaskData(task);
+    }
+
+    // const handleSubmit = submit => {
+    //     setSubmit(submit)
+    // }
+
+
     return (
         <>
-            <tr>
-                <th>{index + 1}</th>
-                <td>{task?.taskName}</td>
-                <td>{task?.taskSerial}</td>
-                <td><label for="details-modal"
-                    onClick={() => setModalDetails(task)}
-                    className=' bg-blue-700 border-0 px-3 py-1 button  rounded text-white modal-button' >Details </label></td>
-                <td>{task.questionDeliverDate},{task.questionDeliverHour}</td>
-                <td>{task.taskDeadline}</td>
+            <div>
+                <div className='py-2 '>
+                    <div>
 
-                <td><label htmlFor="my-modal-3" className="bg-blue-700 border-0 px-3 py-1 button  rounded text-white modal-button" onClick={() => setModal(task)}>Submit</label></td>
-            </tr>
+                        <div>
+                            <div
+                                onClick={() => setToogle(toogle)}
+                                // onClick={() => setSubmit(false)}
+                                // onClick={() => setSubmitOne(true)}
+                                tabindex="0" class="collapse group collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                                {/* <input type="checkbox" class="peer" /> */}
+                                <div
+                                    onClick={() => {
+                                        handleClick(singleTask)
+                                    }}
+                                    class="collapse-title  font-medium flex justify-between">
+                                    <div>
+                                        {singleTask.taskSerial}  -  {singleTask.taskName}
+                                    </div>
+                                    <div>
+                                        Due {singleTask.taskDeadline}
+                                    </div>
+
+                                </div>
+
+                                <div class="collapse-content">
+                                    <label
+                                        onClick={() => setToogle(!toogle)}
+                                        className="bg-blue-700 border-0 px-3 py-1 button  rounded text-white modal-button">Submit</label>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
         </>
     );
 };
 
 export default SingleTasks;
+
+
+
+
+// //////////////////
+
+// import React from 'react';
+
+// const SingleTasks = ({ task, index, setModalDetails, setModal, setFeedbackModal, taskFeedback, singleMark, marks, setMarks }) => {
+//     return (
+//         <>
+//             <tr>
+//                 <th>{index + 1}</th>
+//                 <td>{task?.taskName}</td>
+//                 <td>{task?.taskSerial}</td>
+//                 <td><label for="details-modal"
+//                     onClick={() => setModalDetails(task)}
+//                     className=' bg-blue-700 border-0 px-3 py-1 button  rounded text-white modal-button' >Details </label></td>
+//                 <td>{task.questionDeliverDate},{task.questionDeliverHour}</td>
+//                 <td>{task.taskDeadline}</td>
+
+//                 <td><label htmlFor="my-modal-3" className="bg-blue-700 border-0 px-3 py-1 button  rounded text-white modal-button" onClick={() => setModal(task)}>Submit</label></td>
+//             </tr>
+//         </>
+//     );
+// };
+
+// export default SingleTasks;
