b78c270f7e6c18638bd4a2bc65f8a754932e91a3
Junayed5 <mdjunayed352003@gmail.com>
2022-08-18 12:16:18 +0600

    updated

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -8,7 +8,7 @@ const Dashboard = () => {
     const emails = user?.email
     const [match, setMatch] = useState([])
     useEffect(() => {
-        fetch(`https://cryptic-stream-86241.herokuapp.com/user/${emails}`,  {
+        fetch(`http://localhost:5000/user/${emails}`,  {
             method: 'GET',
             header: {
                 'content-type': 'application/json'
--- src/Components/StudentAnalytic/StudentAnalytic.js
+++ src/Components/StudentAnalytic/StudentAnalytic.js
@@ -51,7 +51,7 @@ const studentAnalytic = () => {
     const [marks, setMarks] = useState([]);
 
     useEffect(() => {
-        fetch(`https://cryptic-stream-86241.herokuapp.com/allMarks/${email}`)
+        fetch(`http://localhost:5000/allMarks/${email}`)
             .then(res => res.json())
             .then(data => setMarks(data))
     }, [marks])
