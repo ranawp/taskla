99e03bdaa375992311906bb630d916327ca74eff
mozahidsani <mozahidwebmin11d10@gmail.com>
2022-08-18 19:15:49 +0600

    complete

--- src/Components/Home/AboutDeveloper/AboutDeveloper.js
+++ src/Components/Home/AboutDeveloper/AboutDeveloper.js
@@ -3,36 +3,31 @@ import React from 'react';
 const AboutDeveloper = () => {
     return (
         <div className='grid justify-center mt-12 mb-16'>
-        <h1 className="text-2xl text-center text-black font-bold">Our  All <span className="text-primary">Team</span>  Member</h1>
+        <h1 className="text-2xl text-center text-black font-bold">Our   <span className="text-primary">Team</span></h1>
 
 
             {/* .............................................................. */}
             <div className='card  mt-12   grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3  justify-center p-6 gap-5'>
 
 
-           
-                {/* ......................... */}
-                <div class="card w-96 bg-base-100 shadow-xl">
+
+{/* ......................... */}
+<div class="card w-96 bg-base-100 shadow-xl">
                     <figure class="px-10 pt-10">
                     <img class="mask mask-circle w-50%" src="https://placeimg.com/160/160/arch" />
                     </figure>
                     <div class="card-body items-center text-center">
                         <h2 class="card-title">Rana Akash</h2>
                         <p class="text-primary font-bold">CEO & Founder</p>
-                        <p className='text-justify'>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, recusandae!
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, recusandae!
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, recusandae!
-                        
-                        </p>
-                        
+                    
                         <div class="flex justify-center">
-                        <i class="fa-brands fa-facebook text-primary p-4 text-2xl fa-beat-fade"></i>
-                        <i class="fa-brands fa-instagram text-primary p-4 text-2xl fa-beat-fade"></i>
-                        <i class="fa-brands fa-github text-primary p-4 text-2xl  fa-beat-fade"></i>
-                        <i class="fa-brands fa-linkedin text-primary p-4 text-2xl fa-beat-fade"></i>
+                        <i class="fa-brands fa-facebook text-primary p-4 text-2xl "></i>
+                        <i class="fa-brands fa-instagram text-red-500 font-bold p-4 text-2xl "></i>
+                        <i class="fa-brands fa-github text-black p-4 text-2xl  "></i>
+                        <i class="fa-brands fa-linkedin text-primary p-4 text-2xl "></i>
                            
                         </div>
+
                     </div>
                 </div>
                 {/* ......................... */}
@@ -44,18 +39,13 @@ const AboutDeveloper = () => {
                     <div class="card-body items-center text-center">
                         <h2 class="card-title">Masud Rana</h2>
                         <p class="text-primary font-bold">Chief Operating Officer</p>
-                        <p className='text-justify'>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, recusandae!
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, recusandae!
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, recusandae!
-                        
-                        </p>
+                       
                         
                         <div class="flex justify-center">
-                        <i class="fa-brands fa-facebook text-primary p-4 text-2xl fa-beat-fade"></i>
-                        <i class="fa-brands fa-instagram text-primary p-4 text-2xl fa-beat-fade"></i>
-                        <i class="fa-brands fa-github text-primary p-4 text-2xl  fa-beat-fade"></i>
-                        <i class="fa-brands fa-linkedin text-primary p-4 text-2xl fa-beat-fade"></i>
+                        <i class="fa-brands fa-facebook text-primary p-4 text-2xl "></i>
+                        <i class="fa-brands fa-instagram text-red-500 font-bold p-4 text-2xl "></i>
+                        <i class="fa-brands fa-github text-black p-4 text-2xl  "></i>
+                        <i class="fa-brands fa-linkedin text-primary p-4 text-2xl "></i>
                            
                         </div>
                     </div>
@@ -64,106 +54,25 @@ const AboutDeveloper = () => {
 
                      {/* ......................... */}
                      <div class="card w-96 bg-base-100 shadow-xl">
-                    <figure class="px-10 pt-10">
-                    <img class="mask mask-circle w-50%" src="https://placeimg.com/160/160/arch" />
+                    <figure class="px-10 pt-10 w-25%">
+                    <img class="mask mask-circle " src="https://placeimg.com/160/160/arch" />
                     </figure>
                     <div class="card-body items-center text-center">
                         <h2 class="card-title">Mozahid Ahmed</h2>
                         <p class="text-primary font-bold">Software Engineer</p>
-                        <p className='text-justify'>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, recusandae!
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, recusandae!
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, recusandae!
-                        
-                        </p>
+                     
                         
                         <div class="flex justify-center">
-                        <i class="fa-brands fa-facebook text-primary p-4 text-2xl fa-beat-fade"></i>
-                        <i class="fa-brands fa-instagram text-primary p-4 text-2xl fa-beat-fade"></i>
-                        <i class="fa-brands fa-github text-primary p-4 text-2xl  fa-beat-fade"></i>
-                        <i class="fa-brands fa-linkedin text-primary p-4 text-2xl fa-beat-fade"></i>
+                        <i class="fa-brands fa-facebook text-primary p-4 text-2xl "></i>
+                        <i class="fa-brands fa-instagram text-red-500 font-bold p-4 text-2xl "></i>
+                        <i class="fa-brands fa-github text-black p-4 text-2xl  "></i>
+                        <i class="fa-brands fa-linkedin text-primary p-4 text-2xl "></i>
                            
                         </div>
                     </div>
                 </div>
                 {/* ......................... */}
-                     {/* ......................... */}
-                     <div class="card w-96 bg-base-100 shadow-xl">
-                    <figure class="px-10 pt-10">
-                    <img class="mask mask-circle w-50%" src="https://placeimg.com/160/160/arch" />
-                    </figure>
-                    <div class="card-body items-center text-center">
-                        <h2 class="card-title">Junayed Ahmed</h2>
-                        <p class="text-primary font-bold">Web Developer</p>
-                        <p className='text-justify'>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, recusandae!
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, recusandae!
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, recusandae!
-                        
-                        </p>
-                        
-                        <div class="flex justify-center">
-                        <i class="fa-brands fa-facebook text-primary p-4 text-2xl fa-beat-fade"></i>
-                        <i class="fa-brands fa-instagram text-primary p-4 text-2xl fa-beat-fade"></i>
-                        <i class="fa-brands fa-github text-primary p-4 text-2xl  fa-beat-fade"></i>
-                        <i class="fa-brands fa-linkedin text-primary p-4 text-2xl fa-beat-fade"></i>
-                           
-                        </div>
-                    </div>
-                </div>
-                {/* ......................... */}
-                     {/* ......................... */}
-                     <div class="card w-96 bg-base-100 shadow-xl">
-                    <figure class="px-10 pt-10">
-                    <img class="mask mask-circle w-50%" src="https://placeimg.com/160/160/arch" />
-                    </figure>
-                    <div class="card-body items-center text-center">
-                        <h2 class="card-title">Hridoy  Ahmed</h2>
-                        <p class="text-primary font-bold">Web Developer</p>
-                        <p className='text-justify'>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, recusandae!
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, recusandae!
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, recusandae!
-                        
-                        </p>
-                        
-                        <div class="flex justify-center">
-                        <i class="fa-brands fa-facebook text-primary p-4 text-2xl fa-beat-fade"></i>
-                        <i class="fa-brands fa-instagram text-primary p-4 text-2xl fa-beat-fade"></i>
-                        <i class="fa-brands fa-github text-primary p-4 text-2xl  fa-beat-fade"></i>
-                        <i class="fa-brands fa-linkedin text-primary p-4 text-2xl fa-beat-fade"></i>
-                           
-                        </div>
-                    </div>
-                </div>
-                {/* ......................... */}
-                     {/* ......................... */}
-                     <div class="card w-96 bg-base-100 shadow-xl">
-                    <figure class="px-10 pt-10">
-                    <img class="mask mask-circle w-50%" src="https://placeimg.com/160/160/arch" />
-                    </figure>
-                    <div class="card-body items-center text-center">
-                        <h2 class="card-title">Parvez  Ahmed</h2>
-                        <p class="text-primary font-bold">Web Developer</p>
-                        <p className='text-justify'>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, recusandae!
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, recusandae!
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, recusandae!
-                        
-                        </p>
-                        
-                        <div class="flex justify-center">
-                        <i class="fa-brands fa-facebook text-primary p-4 text-2xl fa-beat-fade"></i>
-                        <i class="fa-brands fa-instagram text-primary p-4 text-2xl fa-beat-fade"></i>
-                        <i class="fa-brands fa-github text-primary p-4 text-2xl  fa-beat-fade"></i>
-                        <i class="fa-brands fa-linkedin text-primary p-4 text-2xl fa-beat-fade"></i>
-                           
-                        </div>
-                    </div>
-                </div>
-                {/* ......................... */}
-
-                
+                     
 
 
 
@@ -178,3 +87,8 @@ const AboutDeveloper = () => {
 };
 
 export default AboutDeveloper;
+
+
+
+
+                
