757bafebe931ab64df51cf493c03053b50ece355
Junayed5 <mdjunayed352003@gmail.com>
2022-08-18 12:36:10 +0600

    update announcement

--- src/Components/Announcement/Announcement.js
+++ src/Components/Announcement/Announcement.js
@@ -23,9 +23,9 @@ const Announcement = () => {
 
     const announcementFilter = data.filter(a => (a.announcement && a.announcement));
     return (
-        <div class="hero h-[600px] bg-base-200">
-            <div class="hero-content flex-col lg:flex-row">
-                <div className='flex flex-col gap-4 w-[500px]'>
+        <div class="hero bg-base-200 ">
+            <div class="hero-content flex-col lg:flex-row mt-10">
+                <div className='flex flex-col gap-4 w-[500px] overflow-y-auto h-96'>
                     {announcementFilter.map(announcement => <Detail
                         key={announcement._id}
                         announcement={announcement}
@@ -33,10 +33,10 @@ const Announcement = () => {
                     />)}
                 </div>
                 <div>
-                    <div className="card flex-shrink-0 w-[500px] max-w-sm shadow-2xl bg-base-100 mx-auto h-[500px]">
+                    <div className="card flex-shrink-0 w-[700px] max-w-sm shadow-2xl bg-base-100 mx-auto h-[500px]">
                         <div className="card-body">
                             <h2 class="card-title">{notice}</h2>
-                            <p>{announcement    }</p>
+                            <p>{announcement}</p>
                         </div>
                     </div>
                 </div>
