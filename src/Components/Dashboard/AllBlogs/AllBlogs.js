382b6094525b013def0b1d059c32a5ff05534fb0
parvez13913 <parvezz13913@gmail.com>
2022-08-21 02:58:51 -0700

    Edit blog complete

--- src/Components/Dashboard/AllBlogs/AllBlogs.js
+++ src/Components/Dashboard/AllBlogs/AllBlogs.js
@@ -1,20 +1,31 @@
 import React from 'react';
+import { useState } from 'react';
 import UseNewBlog from '../../../hooks/UseNewBlog';
 import AllBlog from './AllBlog';
+import EditBlogModal from './EditBlogModal';
 
 const AllBlogs = () => {
     const [newBlogs, setNewBlogs] = UseNewBlog();
+    const [blogEditModal, setEditModal] = useState(null);
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-3 ml-20 my-5'>
+        <>
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-3 ml-20 my-5'>
+                {
+                    newBlogs.map(allBlog => <AllBlog
+                        key={allBlog._id}
+                        allBlog={allBlog}
+                        setNewBlogs={setNewBlogs}
+                        newBlogs={newBlogs}
+                        setEditModal={setEditModal}
+                    ></AllBlog>)
+                }
+            </div >
             {
-                newBlogs.map(allBlog => <AllBlog
-                    key={allBlog._id}
-                    allBlog={allBlog}
-                    setNewBlogs={setNewBlogs}
-                    newBlogs={newBlogs}
-                ></AllBlog>)
+                blogEditModal && <EditBlogModal
+                    blogEditModal={blogEditModal}
+                ></EditBlogModal>
             }
-        </div >
+        </>
     );
 };
 
